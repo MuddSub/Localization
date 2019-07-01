fbb75832093d148e1ff38d81b1a4220426eecb47
Daniel Yang <dhyang@hmc.edu>
2019-06-30 22:44:56 -0700

    fixed math

--- Gaussian Localization.py
+++ Gaussian Localization.py
@@ -3,7 +3,7 @@
 
 # given camera angle of the task and the change of angle, create a weight distribution in shape of a cone and shift the weight distribution as camera/sub angle changes
 
-# In[1]:
+# In[6]:
 
 
 import math
@@ -13,7 +13,7 @@ from numpy import array
 import copy
 
 
-# In[2]:
+# In[200]:
 
 
 #NOTE: The whole class can be sped up, if necessary, with vectorizing in numpy
@@ -29,16 +29,16 @@ class particleFilter():
         which we will output by mapping this to [0,360), or whatever the sub takes
         '''
         self.particles = angleAmount
-        self.particleMat = [ 0 for i in range(self.particles) ]
+        self.particleMat = [ 1 for i in range(self.particles) ]
         self.i_to_angle = 360/angleAmount
     
     def resetWeights(self):
-        self.particleMat = [ 0 for i in range(self.particles) ]
+        self.particleMat = [ 1 for i in range(self.particles) ]
         
     def addTask(self):
         pass
     
-    def updateAngle(self,newAngle,stdev):
+    def updateAngle(self,newAngle,stdev,mode):
         '''
         data will be the input data in angles, most likely one single input and error in angle. 
         angle might need adjustment to fit our coordinates.
@@ -47,21 +47,28 @@ class particleFilter():
         '''
         increase prob for "angle", bc we see angle.
         '''
+        if mode == 'angle':
+            if newAngle <= 319:
+                newAngle = -math.degrees(np.arctan((319.5-newAngle)/725.24))
+            else:
+                newAngle = math.degrees(np.arctan((newAngle-319.5)/725.24))
+                
         #Might want to change to adust number of measurements to change weights
-        fWeight = 0.3
+        fWeight = 0.09
         
         for i in range(len(self.particleMat)):
             angle = int(round(i*self.i_to_angle))
             angleDiff = min([math.fabs(angle - newAngle),math.fabs(newAngle-angle+360)])
-            gaussDelta =  math.e**( -angleDiff*angleDiff / (2* stdev**2) )
+            gaussDelta =  math.e**( -angleDiff*angleDiff / (2* stdev**2) )*360/math.sqrt(2*math.pi*stdev**2)
             self.particleMat[i] = fWeight*gaussDelta+ (1-fWeight)*self.particleMat[i]
         
         #normalize
-        if sum(self.particleMat)>360:
-            total = 360/sum(self.particleMat)
+        if np.sum(self.particleMat)>360:
+            total = 360/np.sum(self.particleMat)
             self.particleMat = [i*total for i in self.particleMat]
         
-    def updateCameraAngle(self,cameraChange):
+    def updateSubmarineAngle(self,cameraChange):
+        cameraChange = cameraChange % 360
         newAng = [0 for i in range(len(self.particleMat))]
         for i in range(len(self.particleMat)):
             angIdx = int(round(cameraChange/self.i_to_angle))
@@ -87,27 +94,32 @@ class particleFilter():
 #    
 # We use quantity 3 to evaluate "accumulation," or whether we had enough measurements to start predicting angle data.
 # 
-# Once the weight at angle is higher than, say, 120, we next use quantity 2 to calculate "certainty." I suggest, based of very crude estimate, a weight of .97 as a threshold of certainty.
+# Once the weight at angle is higher than, say, 120, we next use quantity 2 to calculate "certainty." I suggest, based of very crude estimate, a weight of 2.75? as a threshold of certainty.
 # 
 # Our idea is to turn toward that weight, then go forward. Once the angle gets too far from origin, we reset the angle matrix.
 
-# In[6]:
+# In[201]:
 
-#SAMPLE:
-'''
+
 particle = particleFilter(360)
-a=particle.updateAngle(90,50)
-for i in range(10):
-    a=particle.updateAngle(90,50)
-for i in range(100):
-    a=particle.updateAngle(45,50)
-    a=particle.updateAngle(0,50)
+for i in range(15):
+    a=particle.updateAngle(0,50,'angle')
+    a=particle.updateAngle(0,50,'angle')
     
+a = particle.updateSubmarineAngle(-37)
     
 for i in range(359):
     plt.polar(i/180*math.pi, particle.particleMat[i],"bo")
 print(particle.getPredictedState())
-'''
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 
